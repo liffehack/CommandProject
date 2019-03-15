abfb125e10195b0e34761052d29f82915d7b8626
Maks Fedorov <maksfedorovval@gmail.com>
2019-03-15 16:14:38 +0300

    Changes Program.cs Fedorov

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Proj
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!HHHH");
         }
     }
 }
